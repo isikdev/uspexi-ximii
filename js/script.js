67b9d6f91e02c424a9fb4642417fb450d7646fad
Искандар <105116560+isikdev@users.noreply.github.com>
2022-07-14 17:24:31 +0500

    100%

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ $('.news__slider').slick({
     dots: true,
     arrows: false
 });
+$(document).ready(function() {
+    $('.info__card-head').on('click', f_acc);
+  });
+  
+  function f_acc(){
+    $('.info__card-body').not($(this).next()).slideUp(100);
+      $(this).next().slideToggle(100);
+  }
